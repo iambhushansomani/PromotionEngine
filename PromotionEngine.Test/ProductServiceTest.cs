03d5a46116728353eeb969f4aec5aeee3b03e623
bhushan.somani <bhushan.somani@external.marelli.com>
2021-06-03 15:00:02 +0530

    Added testcases and logic in program.cs logic

--- PromotionEngine.Test/ProductServiceTest.cs
+++ PromotionEngine.Test/ProductServiceTest.cs
@@ -13,22 +13,73 @@ namespace PromotionEngine.Test
     {
 
         private ProductService ProductService;
+        private List<Product> products;
+            
 
         [SetUp]
         public void SetUp()
         {
             ProductService = new ProductService();
+            products = new List<Product>();
+
         }
 
         [TestCase("A", 50)]
         [TestCase("B", 30)]
         [TestCase("C", 20)]
         [TestCase("D", 15)]
-        public void GetPriceByType(string id,int expectedresult)
+        public void Test_GetPriceByType(string id,int expectedresult)
         {
             var result = ProductService.GetPriceByType(id);
 
             Assert.AreEqual(result, expectedresult);
+        }
+
+        [Test]
+        public void Test_GetTotalPrice_NoPromo()
+        {
+
+           
+            List<string> productlist = new List<string>() { "A", "B", "C" };
+            foreach (var item in productlist)
+            {
+                Product p = new Product();
+                p.id = item;
+                products.Add(p);
+            }
+            int result = ProductService.GetTotalPrice(products);
+            Assert.AreEqual(100, result);
+            
+        }
+
+        [Test]
+        public void Test_GetTotalPrice_Promo1()
+        {
+            List<string> productlist = new List<string>() { "A", "A", "A", "A", "A","B", "B", "B", "B", "B","C" };
+            foreach (var item in productlist)
+            {
+                Product p = new Product();
+                p.id = item;
+                products.Add(p);
+            }
+            int result = ProductService.GetTotalPrice(products);
+            Assert.AreEqual(370, result);
+
+        }
+
+        [Test]
+        public void Test_GetTotalPrice_Promo2()
+        {
+            List<string> productlist = new List<string>() {  "A", "A", "A", "B", "B", "B", "B", "B", "C","D" };
+            foreach (var item in productlist)
+            {
+                Product p = new Product();
+                p.id = item;
+                products.Add(p);
+            }
+            int result = ProductService.GetTotalPrice(products);
+            Assert.AreEqual(280, result);
+
         }
 
     }
